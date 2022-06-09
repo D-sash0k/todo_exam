093db8f1f5b21654ab1f3a61d66d6f7ac49d467e
Sasha Demchyshyn <sash0680k@gmail.com>
2022-06-09 22:07:03 +0300

    add css, js

---
+++ todo_exam.js
@@ -0,0 +1,35 @@
+const img = document.querySelector('.for_img');
+const selects = document.querySelector('.select_status');
+
+const watchSelected = () => {
+    selects.style = 'display: block';
+};
+
+const fff = (e) => {  
+ const img = document.querySelector('.img_color');
+ const statusColor = document.querySelector('.status_color');
+ let ePrevious;
+ 
+    if ( ePrevious !=  e.target.value){
+        if(e.target.value === 'in_progress'){
+            img.src = './img/yellow.jpg';
+            statusColor.textContent = 'in progress';
+            statusColor.style = 'background-color: rgb(250, 160, 5);';
+        }
+        if(e.target.value === 'hold'){
+            img.src = './img/grey.jpg';
+            statusColor.textContent = 'hold';
+            statusColor.style = 'background-color: rgb(136, 136, 136);';
+        }
+        if(e.target.value === 'done'){
+            img.src = './img/green.jpg';
+            statusColor.textContent = 'done';
+            statusColor.style = 'background-color: rgb(28, 201, 109);';
+        }
+        selects.style = 'display: none';
+    }       
+   ePrevious = e.target.value;  
+}
+
+selects.addEventListener('change', fff);
+img.addEventListener('click', watchSelected);
